e56d0ec642786eea52ddd54fd0ffd043594c6e3c
sean stephen brian <seanstephenbrian@gmail.com>
2022-09-23 15:20:26 -0400

    Create last card which triggers addBook() on click

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLog = [
-    {title: '1984', author: 'George Orwell', pages: 328, rating: 2, review: 'not great'},
+    {title: '1984', author: 'George Orwell', pages: 328, rating: 2},
     {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', pages: 208, rating: 1},
     {title: 'Mrs Dalloway', author: 'Virginia Woolf', pages: 194, rating: 3},
     {title: 'Mrs Dalloway', author: 'Virginia Woolf', pages: 194, rating: 5},
@@ -29,16 +29,15 @@ let myLog = [
     {title: 'Mrs Dalloway', author: 'Virginia Woolf', pages: 194}
 ];
 
-function Book(title, author, pages, rating, review) {
+function Book(title, author, pages, rating) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.rating = rating;
-    this.review = review;
 }
 
-function addBookToLog(title, author, pages, rating, review) {
-    let newBook = new Book(title, author, pages, rating, review);
+function addBookToLog(title, author, pages, rating) {
+    let newBook = new Book(title, author, pages, rating);
     myLog.push(newBook);
 }
 
@@ -64,6 +63,13 @@ function generateLog(log) {
             bookProperty.innerHTML = `<div>${property}:</div><div>${book[property]}</div>`;
         };
     });
+    const addBook = logContainer.appendChild(document.createElement('div'));
+    addBook.classList.add('add-book');
+    addBook.innerHTML = `<img src='img/svg/add.svg' alt='Add a new book' onclick='addBook()'>`;
 }
 
 generateLog(myLog);
+
+function addBook() {
+    
+}
